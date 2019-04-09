11fb69838607123009c6d8b3087e0e2337a233cd
EtienneDel <etienne.delnott@viacesi.fr>
2019-04-09 17:21:59 +0200

    Initialized main windows, all dependencies installed.

---
+++ main.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+})
