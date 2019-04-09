04f5e697bb9df81fa758bab5cd9f57ea0c898293
EtienneDel <etienne.delnott@viacesi.fr>
2019-04-09 18:15:34 +0200

    First buttons and added css file

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -13,4 +13,37 @@ app.on('ready', () => {
         protocol: 'file:',
         slashes: true
     }))
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 })
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [{
+            label: 'Quit',
+            click(){
+                app.quit();
+            }
+        }]
+    }
+];
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+      label: 'Developer Tools',
+      submenu:[
+        {
+          role: 'reload'
+        },
+        {
+          label: 'Toggle DevTools',
+          accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+          click(item, focusedWindow){
+            focusedWindow.toggleDevTools();
+          }
+        }
+      ]
+    });
+  }
